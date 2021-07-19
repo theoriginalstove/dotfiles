185424db1d5b4110c170f7adfd517acdfc99a126
Steven Turturo <steven@ioarmory.com>
2021-07-19 02:35:11 -0700

    Fixed font selection issue

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -5,5 +5,4 @@ killall -q polybar
 
 #launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar.log /tmp/polybar2.log
-polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
-polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+polybar simplebar 2>&1 | tee -a /tmp/polybar1.log & disown
