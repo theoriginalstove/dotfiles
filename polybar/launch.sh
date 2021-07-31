41f6e855e2fd4c8448e9b9bffd2c5b4d2cdc83ab
Steven Turturo <steven@ioarmory.com>
2021-07-31 12:51:00 -0700

    Made a new main bar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -5,8 +5,10 @@ killall -q polybar
 
 #launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar.log /tmp/polybar2.log /tmp/polybar3.log
-polybar left --config=$HOME/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar1.log & disown
-polybar center --config=$HOME/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar2.log & disown 
-polybar right --config=$HOME/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar3.log & disown 
-polybar mode --config=$HOME/.config/polybar/config.ini
+#polybar left --config=$HOME/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar1.log & disown
+#polybar center --config=$HOME/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar2.log & disown 
+#polybar right --config=$HOME/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar3.log & disown 
+#polybar mode --config=$HOME/.config/polybar/config.ini
+
+polybar main --config=$HOME/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar1.log & disown
 
